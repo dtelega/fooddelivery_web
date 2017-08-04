62f01bc77612ff8d0536d3db1eafefb559522f7c
Dmytro Telega <dtelega@e3r3p8.unit.ua>
2017-08-04 13:56:01 +0300

    fix include php

--- index.php
+++ index.php
@@ -1,13 +1,7 @@
-
 <?php
-
 	$view = empty($_GET['view']) ? 'main' : $_GET['view'];
 
-	include($_SERVER['DOCUMENT_ROOT'].'/food/header.php');
-
-	include($_SERVER['DOCUMENT_ROOT'].
-		'/food/pages/'.$view.'.php');
-
-	include($_SERVER['DOCUMENT_ROOT'].'/food/pages/footer.php');
+	include($_SERVER['DOCUMENT_ROOT'].'/fooddelivery_web/header.php');
+	include($_SERVER['DOCUMENT_ROOT'].'/fooddelivery_web/pages/'.$view.'.php');
+	include($_SERVER['DOCUMENT_ROOT'].'/fooddelivery_web/pages/footer.php');
 ?>
-	
